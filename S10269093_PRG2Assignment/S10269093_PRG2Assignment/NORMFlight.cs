d5ce92cf865ba0fa6698609861494dd53c88e8ba
gracechua93 <chua.faithboy@gmail.com>
2025-01-26 22:04:56 +0800

    added NORMFlight.cs

---
+++ S10269093_PRG2Assignment/S10269093_PRG2Assignment/NORMFlight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace S10269093_PRG2Assignment
+{
+    class NORMFlight : Flight
+    {
+        public NORMFlight(string fN, string o, string d, DateTime et, string s) : base(fN, o, d, et) { }
+
+        public override double CalculateFees()
+        {
+            if (Origin == "Singapore (SIN)")
+            {
+                return 500 + 300;
+            }
+            else if (Destination == "Singapore (SIN)")
+            {
+                return 800 + 300;
+            }
+
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "Normal Flight";
+        }
+    }
+}
