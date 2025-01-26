c0dcf444aa3136ce94392a798c0c0f571af139c9
gracechua93 <chua.faithboy@gmail.com>
2025-01-26 22:04:20 +0800

    added CTTFlight.cs

---
+++ S10269093_PRG2Assignment/S10269093_PRG2Assignment/CFFTFlight.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace S10269093_PRG2Assignment
+{
+    class CFFTFlight : Flight
+    {
+        public double RequestFee { get; set; }
+
+        public CFFTFlight() : base() { }
+        public CFFTFlight(string fN, string o, string d, DateTime et, string s, double rF) : base(fN, o, d, et)
+        {
+            RequestFee = rF;
+        }
+
+        public override double CalculateFees()
+        {
+            double fees = 0;
+            if (Origin == "Singapore (SIN)")
+            {
+                fees = 500 + 300;
+            }
+            else if (Destination == "Singapore (SIN)")
+            {
+                fees = 800 + 300;
+            }
+
+            RequestFee = 150;
+            fees += RequestFee;
+            return fees;
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "CFFT Flight";
+        }
+    }
+}
