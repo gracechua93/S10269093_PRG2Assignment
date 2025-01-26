2cdc719fb0e9a5b6d532777fedd9e1184e93e2e4
gracechua93 <chua.faithboy@gmail.com>
2025-01-26 22:04:31 +0800

    added DDJBFlight.cs

---
+++ S10269093_PRG2Assignment/S10269093_PRG2Assignment/DDJBFlight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace S10269093_PRG2Assignment
+{
+    class DDJBFlight : Flight
+    {
+        public double RequestFee { get; set; }
+
+        public DDJBFlight() : base() { }
+
+        public DDJBFlight(string fN, string o, string d, DateTime et, string s, double rF) : base(fN, o, d, et)
+        {
+            RequestFee = rF;
+        }
+
+        public override double CalculateFees()
+        {
+            double fees = 0;
+            if (Origin == "Singapore (SIN)")
+            {
+                fees = 500 + 300;
+            }
+            else if (Destination == "Singapore (SIN)")
+            {
+                fees = 800 + 300;
+            }
+
+            RequestFee = 300;
+            fees += RequestFee;
+            return fees;
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "DDJB Flight";
+        }
+    }
+}
