0510eea26cfff734d9842076ad5e9ff002cbb764
gracechua93 <chua.faithboy@gmail.com>
2025-01-26 22:03:54 +0800

    added airline.cs

---
+++ S10269093_PRG2Assignment/S10269093_PRG2Assignment/Airline.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace S10269093_PRG2Assignment
+{
+    class Airline
+    {
+        // properties
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; } = new Dictionary<string, Flight>();
+
+        public Airline() { }
+        public Airline(string c, string n)
+        {
+            Name = n;
+            Code = c;
+        }
+
+        // methods
+        public bool AddFlight(Flight f)
+        {
+            if (!Flights.ContainsKey(f.FlightNumber))
+            {
+                Flights.Add(f.FlightNumber, f);
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveFlight(Flight f)
+        {
+            return Flights.Remove(f.FlightNumber);
+        }
+
+        public double CalculateFees()
+        {
+            double fees = 0;
+            foreach (Flight flight in Flights.Values)
+            {
+                fees += flight.CalculateFees();
+            }
+            return fees;
+        }
+
+        public override string ToString()
+        {
+            return "Airline Name: " + Name + "\nAirline Code: " + Code;
+        }
+    }
+}
